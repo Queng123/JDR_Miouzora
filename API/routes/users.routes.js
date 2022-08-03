9092dbe4d24d8742f38873cdcf3c7eb180cb1d2b
Quentin BREJOIN <Quentin.brejoin@epitech.eu>
2022-08-03 15:18:33 +0200

    fix(API): docu for users routes

--- API/routes/users.routes.js
+++ API/routes/users.routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { signup,  signin, signout } = require('../controllers/users.controller');
 
-//route to signin with user -> /user/signin
+//route to signin with user -> /users/signin
 router.post('/signin', signin);
 
-//route to signout with user -> /user/signout
+//route to signout with user -> /users/signout
 router.get('/signout', signout);
 
-//route to signup with new user -> /user/signup
+//route to signup with new user -> /users/signup
 router.post('/signup', signup);
 
 module.exports = router;
