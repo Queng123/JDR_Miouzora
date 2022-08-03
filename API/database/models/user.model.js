5c7cc2c63e7ca1e83786b0be8d99bc81e7d8aec0
Quentin BREJOIN <Quentin.brejoin@epitech.eu>
2022-08-03 15:59:40 +0200

    feat(API): add unique username accound feature

--- API/database/models/user.model.js
+++ API/database/models/user.model.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 //user schema -> local prepared for login
 const userSchema = schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   local: {
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
