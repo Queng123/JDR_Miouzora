c42b802fe35516cfd697dd7874095ce1d1553a90
Quentin BREJOIN <Quentin.brejoin@epitech.eu>
2022-08-03 10:48:46 +0200

    feat(API): link app to routes

--- API/app.js
+++ API/app.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const index = require('./routes');
+
 require('./database');
+
+const app = express();
+
+//port to listen (default 3000)
+const port = process.env.PORT || 3000;
+
+// jason parser
+app.use(express.json());
+// url encoded parser
+app.use(express.urlencoded({ extended: true }));
+//request logger
+app.use(morgan('short'));
+app.use(index);
+
+
+
+app.listen(port);
