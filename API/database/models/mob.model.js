78d8bd3f98f65d932c3d251b86b62147fcc999e8
Quentin BREJOIN <Quentin.brejoin@epitech.eu>
2022-08-04 16:54:05 +0200

    update(API): updtate mob model

--- API/database/models/mob.model.js
+++ API/database/models/mob.model.js
@@ -3,8 +3,43 @@ const schema = mongoose.Schema;
 
 //mob schema (test)
 const mobSchema = schema({
-  type: {type: String, required: true, unique: true},
-  default_level: {type: Number, required: true},
+  type: {type: String, required: true},
+  name: {type: String, required: true, unique: true},
+  age: {type: Number},
+  description: {type: String},
+  stats: {
+    health: {type: Number, required: true},
+    soul: {type: Number, required: true},
+    nama: {type: Number, required: true},
+    caracteristics: {
+      comprehension: {type: Number, required: true},
+      fluidityWithEnvironment: {type: Number, required: true},
+      discretion: {type: Number, required: true},
+      conviction: {type: Number, required: true},
+      beauty: {type: Number, required: true},
+    },
+    resistance: {
+      physical: {type: Number, required: true},
+      lofi: {type: Number, required: true},
+      explosions: {type: Number, required: true},
+      hypnosis: {type: Number, required: true},
+      AMI: {type: Number, required: true},
+      souls: {type: Number, required: true},
+      influence: {type: Number, required: true},
+      feelings: {type: Number, required: true},
+      drowning: {type: Number, required: true},
+    },
+    strenght: {
+      physical: {type: Number, required: true},
+      explosif: {type: Number, required: true},
+      hypnosis: {type: Number, required: true},
+      souls: {type: Number, required: true},
+      influence: {type: Number, required: true},
+      feelings: {type: Number, required: true},
+      drowning: {type: Number, required: true},
+      lofif: {type: Number, required: true},
+    }
+  }
 });
 
 const Mob = mongoose.model('mob', mobSchema);
