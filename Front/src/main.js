46526f78ee4700033e1dbc147e0f975b18efffcf
miou-zora <alexandre.franquet@epitech.eu>
2022-08-06 12:46:38 +0200

    chore(Web) : unify main.js

--- Front/src/main.js
+++ Front/src/main.js
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
 library.add(faArrowsRotate)
 
 const app = createApp(App)
